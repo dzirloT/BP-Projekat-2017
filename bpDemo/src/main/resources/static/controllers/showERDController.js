5c12a1f96065dc62903db165eaa3f29e8eeaff71
dakipej <dariopejcinovic@gmail.com>
2017-12-29 18:43:12 +0100

    ispravljen ERD

--- bpDemo/src/main/resources/static/controllers/showERDController.js
+++ bpDemo/src/main/resources/static/controllers/showERDController.js
@@ -3,35 +3,6 @@ app.controller('showERDController', function($scope, $http, $log, download,$root
   erdTables = [];
   erdRelations = [];
 
-  $scope.model = new go.GraphLinksModel(
-    [
-      { key: "Products",
-        items: [ { name: "ProductID", iskey: true, figure: "Decision", color: "purple" },
-                 { name: "ProductName", iskey: false, figure: "Cube1", color: "purple" },
-                 { name: "SupplierID", iskey: false, figure: "Decision", color: "purple" },
-                 { name: "CategoryID", iskey: false, figure: "Decision", color: "purple" } ] },
-      { key: "Suppliers",
-        items: [ { name: "SupplierID", iskey: true, figure: "Decision", color: "purple" },
-                 { name: "CompanyName", iskey: false, figure: "Cube1", color: "purple" },
-                 { name: "ContactName", iskey: false, figure: "Cube1", color: "purple" },
-                 { name: "Address", iskey: false, figure: "Cube1", color: "purple" } ] },
-      { key: "Categories",
-        items: [ { name: "CategoryID", iskey: true, figure: "Decision", color: "purple" },
-                 { name: "CategoryName", iskey: false, figure: "Cube1", color: "purple" },
-                 { name: "Description", iskey: false, figure: "Cube1", color: "purple" },
-                 { name: "Picture", iskey: false, figure: "TriangleUp", color: "purple" } ] },
-      { key: "Order Details",
-        items: [ { name: "OrderID", iskey: true, figure: "Decision", color: "purple" },
-                 { name: "ProductID", iskey: true, figure: "Decision", color: "purple" },
-                 { name: "UnitPrice", iskey: false, figure: "MagneticData", color: "purple" },
-                 { name: "Quantity", iskey: false, figure: "MagneticData", color: "purple" },
-                 { name: "Discount", iskey: false, figure: "MagneticData", color: "purple" } ] }
-    ],
-    [
-      { from: "Products", to: "Suppliers", text: "0..N", toText: "1" },
-      { from: "Products", to: "Categories", text: "0..N", toText: "1" },
-      { from: "Order Details", to: "Products", text: "0..N", toText: "1" }
-    ]);
     $http.post("http://localhost:8080/jdbc/showERD").then(
       function successResponse(succResponse)  {
         $log.log("Radi showERD");
